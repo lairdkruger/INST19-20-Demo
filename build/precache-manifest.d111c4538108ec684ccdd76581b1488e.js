019a708570150c7140f0f5c752d24ac14a42fcdf
lkr27 <lkr27@uclive.ac.nz>
2020-07-16 16:11:06 +1200

    build

---
+++ build/precache-manifest.d111c4538108ec684ccdd76581b1488e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7045d1e46c8ecc207c5947015facd361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "786615cbabd31582169a",
+    "url": "/static/css/main.5e82da85.chunk.css"
+  },
+  {
+    "revision": "44a1442a80101c3f6a47",
+    "url": "/static/js/2.d42a2f4e.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.d42a2f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786615cbabd31582169a",
+    "url": "/static/js/main.46a95a66.chunk.js"
+  },
+  {
+    "revision": "0326d15c0ca36f28de8a",
+    "url": "/static/js/runtime-main.99d90f81.js"
+  }
+]);
